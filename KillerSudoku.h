43cb049898efa681fa415bb8c327b65adb1f4fbe
Heimidall <pedroxlda12@gmail.com>
2018-12-09 12:27:10 -0300

    Grilla lista (arreglada despues del cagaso)

--- KillerSudoku.h
+++ KillerSudoku.h
@@ -4,19 +4,32 @@
 #include <cstdlib>
 #include <vector>
 #include <sstream>
-#include "celda.h"
 using namespace std;
+class Celda{
+  public:
+    size_t size = 9;
+    int valor_inicial;
+    int sector;
+    int sector_value;
+    vector<int> dominio;
+    Celda(){
+      for (size_t i = 0; i < size; i++) {
+        dominio.push_back(i+1);
+      }
+    }
+    void printdom(){
+      for(int i = 0; i < 9; i++) {
+        cout << dominio[i] << '\n';
+      }
+    }
+};
 
 class KS{
 public:
+  vector<vector<Celda>> matrix{9, vector<Celda>(9)};
+  vector<int> valor_sectores;
+  vector<int>::iterator it;
   KS(){
-
-    vector<vector<Celda>> matrix(9, vector<Celda>(9));
-    vector<int> valor_sectores;
-    //matrix[0][1].sector_value = 25;
-    //matrix[2][2].sector_value = 99;
-    string s;
-    char valor;
     char ptos;
     string value;
     int pos,x;
@@ -27,7 +40,7 @@ public:
       for (size_t j = 0; j < 9; j++) {
         file >> ptos;
         if(ptos == '.'){
-          matrix[i][j].valor_inicial = -1;
+          matrix[i][j].valor_inicial = 0;
         }
         else{
           x = ptos - '0';
@@ -38,12 +51,8 @@ public:
         c = value.substr(pos+1);
         stringstream ent(c);
         ent >> matrix[i][j].sector;
-        //file >> matrix[i][j].sector;
-        cout << matrix[i][j].valor_inicial << "C" << matrix[i][j].sector << '\n';
-      }
-      cout << "Aca termina la matriz" << '\n';
-    }
-    cout  << "aca empieza los valores de los sectores" << '\n';
+        }
+  }
     while(file.good()){
       file >> value;
       pos = value.find(':');
@@ -51,8 +60,22 @@ public:
       stringstream ent(c);
       ent >> x;
       valor_sectores.push_back(x);
-      }
-      cout << matrix[0][0].sector << " " <<valor_sectores[(matrix[0][0].sector)-1] << '\n';
+    }
     file.close();
   }
+
+  void printGrilla(){
+    for (size_t i = 0; i < 9; i++) {
+      for (size_t j = 0; j < 9; j++) {
+        cout << matrix[i][j].valor_inicial << " ";
+      }
+      cout << '\n';
+    }
+  }
+  void printSectores(){
+    for ( it = valor_sectores.begin() ; it != valor_sectores.end(); ++it)
+      cout << ' ' << *it;
+    cout << '\n';
+  }
+
 };
--- prueba.cpp
+++ prueba.cpp
@@ -4,12 +4,19 @@
 #include <cstdlib>
 #include <vector>
 #include <sstream>
-#include "celda.h"
 #include "KillerSudoku.h"
 using namespace std;
 
 int main(void) {
   KS sudoku;
+  sudoku.printGrilla();
+  sudoku.printSectores();
+  /*sudoku.FiltroDominicial(sudoku);
+  for (size_t i = 0; i < 9; i++) {
+    for (size_t j = 0; j < 9; j++) {
+      sudoku.matrix[i][j].printdom();
+    }
+  }*/
 //cout << matrix[0][1].sector_value << "este deberia ser 2,5" << matrix[2][2].sector_value << "este deberia ser 9,9";
 return 0;
 }
